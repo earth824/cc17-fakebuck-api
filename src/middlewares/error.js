4f0a91c38004337a3a9c3b006ffdac7bf0a7b201
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2024-05-29 17:50:28 +0700

    config jwt error status code

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,5 +1,11 @@
+const { JsonWebTokenError, TokenExpiredError } = require('jsonwebtoken');
+
 const errorMiddleware = (err, req, res, next) => {
   console.log(err);
+
+  if (err instanceof JsonWebTokenError || err instanceof TokenExpiredError) {
+    err.statusCode = 401;
+  }
 
   res
     .status(err.statusCode || 500)
